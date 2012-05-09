44a40490c1e5760bb3b98f38303545ed3e87f178
Steve Winton <stevewinton@gmail.com>
2012-05-09 17:14:05 +0100

    Incoming email configured

---
+++ handle_incoming_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import logging, email
+
+from google.appengine.ext import webapp 
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler 
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class IncomingMailHandler(InboundMailHandler):
+    """
+    Handles all incoming mail.
+    """
+    def receive(self, message):
+        plaintext_bodies = message.bodies('text/plain')
+        
+        # Log the sender and message body...
+        logging.info("Received message from %s: %s" % (message.sender, message.bodies('text/plain')))
+        
+        # Send a response...
+        # TODO...
+    
+
+application = webapp.WSGIApplication([IncomingMailHandler.mapping()], debug=True)
+run_wsgi_app(application)
